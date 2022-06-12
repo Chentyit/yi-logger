63891944cb348da6fc25a99e07ad19a1aba38f22
chentianyi <chentyit@163.com>
2022-06-12 13:26:56 +0800

    update: 在写入文件时判断文件大小，并进行归档和压缩操作

--- file_op/file.go
+++ file_op/file.go
@@ -1,7 +1,11 @@
 package file_op
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -10,22 +14,16 @@ type FileOp struct {
 	isOpen       bool // 用于判断是否可以进行操作
 	needCompress bool // 是否需要压缩
 	maxSize      int  // 以 MB 为单位
-	curSize      int
-	maxAge       int // 以天为单位
-	maxBackups   int
 	curDate      time.Time
 	path         string
 }
 
-func CreateFileOp(path string, maxSize int, maxAge int, maxBackups int, needCompress bool) *FileOp {
+func CreateFileOp(path string, maxSize int, needCompress bool) *FileOp {
 	return &FileOp{
 		path:         path,
 		needCompress: needCompress,
 		isOpen:       false,
-		curSize:      0,
 		maxSize:      maxSize,
-		maxAge:       maxAge,
-		maxBackups:   maxBackups,
 	}
 }
 
@@ -60,8 +58,57 @@ func (fo *FileOp) Write(buf []byte) error {
 		_ = fo.ready()
 	}
 
+	var wg sync.WaitGroup
+
+	// 判断当前文件是否超出 maxSize
+	// 如果超出了最大限制，则需要进行以下操作:
+	// - 断开 fo.file 指针
+	// - 创建新文件，并将 fo.file 指向新的文件
+	// - 将原来的文件压缩打包
+	if fo.overMaxSize() {
+		_ = fo.Close()
+
+		now := time.Now()
+
+		date, month, day := now.Date()
+		timestamp := now.Unix()
+
+		// 获取原文件路径
+		filePreDir := filepath.Dir(fo.path)
+		// 获取原文件名称和扩展名
+		fileNameAndExt := strings.Split(filepath.Base(fo.path), ".")
+		fileName := fileNameAndExt[0]
+		fileExt := fileNameAndExt[1]
+		// 拼接新文件名（fileName-year-month-day-timestamp.fileExt)
+		changeFileName := fmt.Sprintf("%s-%v-%v-%v-%v.%s", fileName, date, int(month), day, timestamp, fileExt)
+		// 先改名再压缩是为了防止数据写入时因为压缩速度太慢而造成阻塞
+		changeFilePath, err := ChangeFileName(fo.path, changeFileName)
+		if err != nil {
+			return err
+		}
+		// 重新初始化 fo.file 继续写
+		_ = fo.ready()
+
+		// 判断用户是否设置压缩
+		if fo.needCompress {
+			// 使用同步锁保证压缩过程不会中断
+			wg.Add(1)
+			go func() {
+				wg.Done()
+				pkgName := fmt.Sprintf("%s-%v-%v-%v-%v.%s", fileName, date, int(month), day, timestamp, "zip")
+				pkgPath := filepath.Join(filePreDir, pkgName)
+				_ = Compress(pkgPath, changeFilePath)
+				// 删除原文件
+				_ = Remove(changeFilePath)
+			}()
+		}
+	}
+
 	buf = append(buf, '\n')
 	_, err := fo.file.Write(buf)
+
+	// 等待压缩完成
+	wg.Wait()
 	return err
 }
 
